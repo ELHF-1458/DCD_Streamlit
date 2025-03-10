db9afd0ce3053dd3e63710be1e76bcce791b80a7
ELHF-1458 <houssein1458@gmail.com>
2025-03-10 18:15:53 +0000

    Update git_dcd.py

--- git_dcd.py
+++ git_dcd.py
@@ -8,7 +8,8 @@ import datetime
 from io import BytesIO
 import warnings
 
-warnings.filterwarnings("ignore")
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
 
 
 # -----------------------------------------
