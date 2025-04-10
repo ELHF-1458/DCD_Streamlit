2f8661e61111458bae36a3405d82cc8cefe343b1
ELHF-1458 <houssein1458@gmail.com>
2025-04-10 12:32:24 +0100

    Update git_dcd.py

--- git_dcd.py
+++ git_dcd.py
@@ -226,7 +226,8 @@ if uploaded_file is not None:
     # Utilisation de la fonction de chargement qui nettoie également "Prestataire"
     df_original = load_data_from_uploaded(uploaded_file)
     st.success("Fichier d'origine chargé avec succès.")
-    # st.write(df_original["Cout"].unique())
+    st.write(df_original["Cout"].unique())
+    st.write(df_original["Valeur"].unique())
 
 else:
     st.info("Veuillez uploader votre fichier CSV d'origine.")
